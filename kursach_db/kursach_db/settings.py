a4cfe9dfc9fd48f83cde2d14774c77af323dd974
User <sheparov.71@mail.ru>
2023-06-22 17:14:51 +0300

    add debug_toolbar

--- kursach_db/kursach_db/settings.py
+++ kursach_db/kursach_db/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
 
     'auth_profile.apps.AuthProfileConfig',
     'job_app.apps.JobAppConfig',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'kursach_db.urls'
@@ -139,3 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- kursach_db/kursach_db/urls.py
+++ kursach_db/kursach_db/urls.py
@@ -13,21 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 import mimetypes
 
-mimetypes.add_type("application/javascript", ".js", True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('auth_profile.urls')),
     path('vacancy/', include('job_app.urls')),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
 
 if settings.DEBUG:
     urlpatterns.extend(
         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
+
+mimetypes.add_type("application/javascript", ".js", True)
